be9b08c0231ade4a74f5c182b050a79c0049cf3a
goreleaserbot <goreleaser@carlosbecker.com>
2018-03-08 01:42:54 -0600

    tt version 0.8.9

--- Formula/tt.rb
+++ Formula/tt.rb
@@ -1,9 +1,9 @@
 class Tt < Formula
   desc ""
   homepage ""
-  url "https://github.com/bgetsug/tt/releases/download/0.8.8/tt_0.8.8_macOS_x86_64.tar.gz"
-  version "0.8.8"
-  sha256 "8be7ce1176c5929f28552d507f0282b3f1da4ebc790e5990aeaa07c3ad58af56"
+  url "https://github.com/bgetsug/tt/releases/download/0.8.9/tt_0.8.9_macOS_x86_64.tar.gz"
+  version "0.8.9"
+  sha256 "6341224dc936eeb45828b638094bf95b1859253f3420949bf54de008b2812865"
 
   def install
     bin.install "tt"
